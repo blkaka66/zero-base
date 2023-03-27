08cc2e8f0dbd8c5b2c63fe0961b18840caae24f3
rpdgh <gx53q11@naver.com>
2023-03-27 22:47:21 +0900

    0327

---
+++ withoutCRA/src/components/FollowButton.js
@@ -0,0 +1,8 @@
+// React Component!
+// Follow <-> following
+function FollowButton() {
+    return React.createElement("div", {}, "Follow");
+  }
+  const domContainer = document.querySelector('#follow_button_container');
+  
+  ReactDom.render(React.createElement(FollowButton),domContainer);
